69461a106160093402e62bea984312bd0633a13a
jinhwa <slzp0119@naver.com>
2021-01-30 13:40:33 +0900

    2-5 Updating Header part1 ( console Test )

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -7,11 +7,24 @@ import Favs from "../screens/Favs";
 
 const Tabs = createBottomTabNavigator();
 
-export default () => (
-    <Tabs.Navigator>
+export default (props) => {
+    // navigation.setOptions({title: "Hello from the Tabs"});
+    // console.log(navigation); 이렇게 콘솔에 검색해서 보면 사용가능한 (?) 가지고 있는 많~~~은 option들을 확인 할 수 있다. 
+    console.log(props);
+    return (
+        <Tabs.Navigator>
         <Tabs.Screen name="Movies" component={Movies} />
-        <Tabs.Screen name="Tv" component={Tv} />
+        <Tabs.Screen name="TV" component={Tv} />
         <Tabs.Screen name="Search" component={Search} />
         <Tabs.Screen name="Favs" component={Favs} />
-    </Tabs.Navigator>
-);
+        </Tabs.Navigator>
+    );
+};
+// name 값들은 tab의 항목 이름이다. 
+
+// navigetor 안에 있는 모든 screen은 navigation prop을 갖는다. 그리고, 그 navigation prop은 나에게 조종하게 
+// 허용하게 하는것 뿐만 아니라, 부모 navigator랑 소통할 수 있게도 해준다. 
+// 즉, Stack.js 에 있는 tab요소들이 부모가 되는것. 
+// 지금 tab navigation이 stack navigation의 자식이기 때문에 그 tab navigation은 prop navigation을 갖는다. 
+// 그리고 이건 우리가 몇몇 옵션들을 지정할 수 있다는 의미이다. 
+// 그리고 이 옵션들 중 하나는, navigation.setOption({})
