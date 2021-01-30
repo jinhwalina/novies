fc016c35b226133ff4329b7e835ee6f6af301326
jinhwa <slzp0119@naver.com>
2021-01-30 14:00:12 +0900

    2-6 Updating Header part2

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -4,13 +4,20 @@ import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import Favs from "../screens/Favs";
+import { useLayoutEffect } from 'react';
 
 const Tabs = createBottomTabNavigator();
 
-export default (props) => {
+const getHeaderName = (route) => route?.state?.routeNames[route.state.index] || "Movies";
+
+export default ({navigation, route}) => {
+    useLayoutEffect(() => {
+       // console.log("the route changed") 이렇게 콘솔에 확인해준 이유는, 화면에서 tab을 할 경우 ( 각 페이지에 해당하는 항목의 tab ) 누를 때마다 콘솔에 찍힌다. 
+       navigation.setOptions({title: getHeaderName(route)});
+    }, [route]);
     // navigation.setOptions({title: "Hello from the Tabs"});
     // console.log(navigation); 이렇게 콘솔에 검색해서 보면 사용가능한 (?) 가지고 있는 많~~~은 option들을 확인 할 수 있다. 
-    console.log(props);
+    // console.log(props);
     return (
         <Tabs.Navigator>
         <Tabs.Screen name="Movies" component={Movies} />
