cb3b990b70b5a08dc7f408b5d12b4727399f40e5
jinhwa <slzp0119@naver.com>
2021-01-30 11:29:27 +0900

    2-1 cacheImages

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import AppLoading from "expo-app-loading"; // apploading import 해오지 못하는 문제 해결 import 문구 수정하고, npm install 해줬다.
+import { Text, Image } from "react-native";
+import { Asset } from 'expo-asset';
+
+const cacheImages = (images) => images.map(image => {
+  if(typeof image === "string"){
+    return Image.prefetch(image)
+  } else {
+    return Asset.fromModule(image).downloadAsync();
+  }
+})
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Yeah!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [isReady, setisReady] = useState(false);
+  const loadAssets = async () => { 
+    const images = cacheImages( // images는 promise 들의 array다. 
+      ["https://images.unsplash.com/photo-1552053831-71594a27632d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=912&q=80",
+      require("./assets/splash.png")
+    ]);
+    console.log(images);
+  };
+  const onFinish = () => setisReady(true);
+  return isReady ? <Text>I'm not ready..!!!!!</Text> : <AppLoading startAsync={loadAssets} onFinish={onFinish} onError={console.error}/>; 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
