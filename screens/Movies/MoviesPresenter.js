fe5ee04ac82a5316ee5fdb6454574134439cc6f9
jinhwa <slzp0119@naver.com>
2021-01-31 19:29:17 +0900

    4-3 Slider Content

--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -1,14 +1,20 @@
 import React from "react";
 import styled from 'styled-components/native';
 import Swiper from "react-native-web-swiper";
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, View, Dimensions } from 'react-native';
 import Slide from "../../components/Movies/Slide";
 
+const {width:WIDTH, height:HEIGHT} = Dimensions.get("screen");
 
 const Container = styled.View`
     flex: 1;
     background-color: black;
     justify-content: center;
+`;
+
+const SliderContainer = styled.View`
+    width: ${WIDTH}px;
+    height: ${HEIGHT / 4}px;
 `;
 
 
@@ -19,20 +25,20 @@ export default ({ loading, nowPlaying }) => (
             <ActivityIndicator color="white" size="small" />
         ) : (
             // 이렇게 적어주게 되면 각 슬라이더에 nowPlaying 요소가 담긴다! 
-            <>
+            <SliderContainer>
                 <Swiper controlsEnabled={false} loop timeout={3}>
                     {nowPlaying.map(movie => (
                         <Slide 
                             key={movie.id} 
                             id={movie.id} 
-                            title={movie.original_language}
+                            title={movie.original_title}
                             overview={movie.overview}
                             votes={movie.vote_average}
                             backgroundImage={movie.backdrop_path}
                         />
                     ))}
                 </Swiper>
-            </>
+            </SliderContainer>
         )}
     </Container>
 );
