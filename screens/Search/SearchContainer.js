c8d4c1d13f5ff32ba227a828cca92780719807cf
jinhwa <slzp0119@naver.com>
2021-02-03 14:07:43 +0900

    6-1 Searching Logic

--- screens/Search/SearchContainer.js
+++ screens/Search/SearchContainer.js
@@ -1,4 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { movieApi, tvApi } from '../../api';
 import SearchPresenter from "./SearchPresenter";
 
-export default () => <SearchPresenter />;
+export default () => {
+    const [keyword, setkeyword] = useState("");
+    const [results, setresults] = useState({
+        movies: [],
+        shows: [],
+        movieError: null,
+        showsError: null
+    })
+    const onChange = (text) => setkeyword(text);
+    // const onSubmit = () => console.log("search for", keyword);
+    const search = async () =>{ // search는 async가 되어야 한다.
+        const [movies, movieError] = await movieApi.search(keyword);
+        const [shows, showsError] = await tvApi.search(keyword);
+        setresults({
+            movies,
+            shows,
+            movieError,
+            showsError
+        });
+    };
+    console.log(results);
+    return (
+        <SearchPresenter 
+            {...results}
+            onChange={onChange}
+            onSubmit={search} 
+            keyword={keyword}
+        />
+    );
+};
--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -1,13 +1,26 @@
 import React from "react";
 import styled from "styled-components/native";
+import Horizontal from '../../components/Horizontal';
+import HorizontalSlider from '../../components/HorizontalSlider';
 import Input from '../../components/Search/Input';
 
 const Container = styled.ScrollView`
     background-color: black;
 `;
 
-export default () => (
+export default ({ movies, shows, keyword, onChange, onSubmit }) => (
     <Container>
-        <Input placeholder={"Write a keyword"}/>
+        <Input 
+            placeholder={"Write a keyword"}
+            value={keyword}
+            onChange={onChange}
+            onSubmit={onSubmit}
+        />
+        <HorizontalSlider title={"Movie results"}>
+            {movies.map(movie => <Horizontal />)}
+        </HorizontalSlider>
+        <HorizontalSlider title={"TV results"}>
+            {shows.map(show => <Horizontal />)}
+        </HorizontalSlider>
     </Container>
 );
