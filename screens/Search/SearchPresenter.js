30f264bccbf259a1d950da28537a39d937bc059a
jinhwa <slzp0119@naver.com>
2021-02-03 14:36:22 +0900

    6-2 Searching Styles

--- screens/Search/SearchPresenter.js
+++ screens/Search/SearchPresenter.js
@@ -3,24 +3,47 @@ import styled from "styled-components/native";
 import Horizontal from '../../components/Horizontal';
 import HorizontalSlider from '../../components/HorizontalSlider';
 import Input from '../../components/Search/Input';
+import Vertical from '../../components/Vertical';
 
 const Container = styled.ScrollView`
     background-color: black;
 `;
 
 export default ({ movies, shows, keyword, onChange, onSubmit }) => (
-    <Container>
+    <Container contentContainerStyle={{
+        paddingTop:10
+    }}>
         <Input 
             placeholder={"Write a keyword"}
             value={keyword}
             onChange={onChange}
             onSubmit={onSubmit}
         />
-        <HorizontalSlider title={"Movie results"}>
-            {movies.map(movie => <Horizontal />)}
+        {movies.length !== 0 && (
+            <HorizontalSlider title={"Movie results"}>
+                {movies.map(movie => (
+                    <Vertical 
+                        key={movie.id}
+                        id={movie.id}
+                        votes={movie.vote_average}
+                        title={movie.title}
+                        poster={movie.poster_path || movie.backdrop_path}
+                    />
+                ))}
+            </HorizontalSlider>)}
+
+        {shows.length !==  0 && (
+            <HorizontalSlider title={"TV results"}>
+                {shows.map(show => (
+                    <Vertical 
+                        key={show.id}
+                        id={show.id}
+                        votes={show.vote_average}
+                        title={show.name}
+                        poster={show.poster_path || show.backdrop_path}
+                    />
+                ))}
         </HorizontalSlider>
-        <HorizontalSlider title={"TV results"}>
-            {shows.map(show => <Horizontal />)}
-        </HorizontalSlider>
+        )}
     </Container>
 );
--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 // 일정 이상 수의 글자가 넘어가면 처리해주는 함수이다. ~~~~~~~... 이런식으로!
 // 화면에서 너무 자주 쓰여서 그냥 함수를 만들었다. 
-export const trimText = (text, limit) => text.length > limit ? `${text.slice(0,limit)}...` : text;
+export const trimText = (text = "", limit) => text.length > limit ? `${text.slice(0,limit)}...` : text;
 
 // upcoming의 날짜 형식을 한국식으로 바꿔줬다.
 export const formatDate = (date) => {
